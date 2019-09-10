4d4fabb29fceb8206d6f57931caf7bc88c388161
user01 <sachin.saraswat1741@gmail.com>
2019-09-10 08:53:39 -0400

    Added Data validations on BeerDto. Closes #8

--- src/main/java/com/example/sfgbeerservice/web/model/BeerDto.java
+++ src/main/java/com/example/sfgbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -15,19 +19,30 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
 
 
+    @Null
     private OffsetDateTime createDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyleEnum beerStyleEnum;
 
+    @NotNull
+    @Positive
     private Long upc;
 
+    @NotNull
+    @Positive
     private BigDecimal price;
 
     private Integer quantityOnHand;
